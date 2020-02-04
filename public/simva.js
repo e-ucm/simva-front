7e197481817c453bc4711991d3ea372b348e206e
Synpheros <Synpheros@hotmail.com>
2020-02-05 00:17:02 +0100

    Added groups functionality to simva.js

--- public/simva.js
+++ public/simva.js
@@ -32,6 +32,18 @@ var Simva = {
 		Utils.post(this.apiurl + '/groups', body, callback, this.jwt);
 	},
 
+	updateGroup: function(group, callback){
+		Utils.put(this.apiurl + '/groups/' + group._id, group, callback, this.jwt);
+	},
+
+	getGroup: function(group_id, callback){
+		Utils.get(this.apiurl + '/groups/' + group_id, callback, this.jwt);
+	},
+
+	getGroupParticipants: function(group_id, callback){
+		Utils.get(this.apiurl + '/groups/' + group_id + '/participants', callback, this.jwt);
+	},
+
 	// STUDIES
 
 	getStudies: function(callback){
@@ -44,11 +56,11 @@ var Simva = {
 	},
 
 	getStudy: function(study_id, callback){
-		Utils.get(this.apiurl + '/studies/' + id, callback, this.jwt);
+		Utils.get(this.apiurl + '/studies/' + study_id, callback, this.jwt);
 	},
 
 	getStudyTests: function(study_id, callback){
-		Utils.get(this.apiurl + '/studies/' + id + '/tests', callback, this.jwt);
+		Utils.get(this.apiurl + '/studies/' + study_id + '/tests', callback, this.jwt);
 	},
 
 	getTestActivities: function(study_id, test_id, callback){
