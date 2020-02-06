c27bf222bf1084cd0278c926ee7d51af7f50150f
Synpheros <Synpheros@hotmail.com>
2020-02-06 20:02:07 +0100

    Added Progress bars to se completion and results for Activities

--- public/activities/activitypainter.js
+++ public/activities/activitypainter.js
@@ -44,6 +44,9 @@ var ActivityPainter = {
 		$('#test_' + activity.test + ' .activities').append('<div id="activity_' + activity._id + '" class="activity t' + activity.type + '">'
 			+ '<div class="top"><h4>' + activity.name + '</h4>'
 			+ '<input class="red" type="button" value="X" onclick="deleteActivity(\'' + activity._id + '\')"></div>'
+			+ '<p>Default Activity</p>'
+			+ '<div id="completion_progress_' + activity._id + '" class="progress"><div class="partial"></div><div class="done"></div><span>Completed: <done>0</done>%</span></div>'
+			+ '<div id="result_progress_' + activity._id + '" class="progress"><div class="partial"></div><div class="done"></div><div></div><span>Results: <partial>0</partial>(<done>0</done>)%</span></div>'
 			+ this.paintActivityParticipantsTable(activity, participants) + '</div>');
 	},
 
@@ -64,21 +67,60 @@ var ActivityPainter = {
 	paintActivityCompletion: function(activity, status){
 		let usernames = Object.keys(status);
 
+		let done = 0;
+
 		for (var i = 0; i < usernames.length; i++) {
-			let completion = '<span class="' + ( !status[usernames[i]] ? 'red' : 'green') + '">' + status[usernames[i]] + '</span>'
+			if(status[usernames[i]]){
+				done++;
+			}
+
+			let completion = '<span>' + status[usernames[i]] + '</span>'
+			$('#completion_' + activity._id + '_' + usernames[i]).addClass(!status[usernames[i]] ? 'red' : 'green');
 			$('#completion_' + activity._id + '_' + usernames[i]).empty();
 			$('#completion_' + activity._id + '_' + usernames[i]).append(completion);
 		}
+
+		let progress = Math.round((done / usernames.length) * 1000) / 10; 
+
+		if(isNaN(progress)){
+			progress = 0;
+		}
+
+		$('#completion_progress_' + activity._id + ' .done').css('width', progress + '%' );
+		$('#completion_progress_' + activity._id + ' done').text(progress);
 	},
 
 	paintActivityResult: function(activity, results){
 		let usernames = Object.keys(results);
 
+		let done = 0, partial = 0;
+
 		for (var i = 0; i < usernames.length; i++) {
-			let completion = '<span class="' + (results[usernames[i]] == 'No results' ? 'red' : 'green') + '">' + results[usernames[i]] + '</span>'
+			let status = (results[usernames[i]] !== 'No results');
+			if(status){
+				done++;
+			}
+
+			let completion = '<span>' + results[usernames[i]] + '</span>'
+			$('#result_' + activity._id + '_' + usernames[i]).addClass(status ? 'green' : 'red');
 			$('#result_' + activity._id + '_' + usernames[i]).empty();
 			$('#result_' + activity._id + '_' + usernames[i]).append(completion);
 		}
+
+		let progress = Math.round((done / usernames.length) * 1000) / 10; 
+		let partialprogress = Math.round((partial / usernames.length) * 1000) / 10;
+
+		if(isNaN(progress)){
+			progress = 0;
+		}
+		if(isNaN(partialprogress)){
+			partialprogress = 0;
+		}
+
+		$('#result_progress_' + activity._id + ' .done').css('width', progress + '%' );
+		$('#result_progress_' + activity._id + ' .partial').css('width', partialprogress + '%' );
+		$('#result_progress_' + activity._id + ' done').text(progress);
+		$('#result_progress_' + activity._id + ' partial').text(partialprogress);
 	}
 }
 
