e95019843fe18b8196da0a3ec29d477fd99f1d43
Synpheros <Synpheros@hotmail.com>
2020-02-06 20:02:38 +0100

    Implemented LimeSurvey Activity completion and status control

--- public/activities/limesurveypainter.js
+++ public/activities/limesurveypainter.js
@@ -8,6 +8,8 @@ if(!PainterFactory){
 }
 
 var LimeSurveyPainter = {
+	limesurveyurl: 'https://limesurvey-dev.external.test/',
+
 	supportedType: 'limesurvey',
 	simpleName: 'LimeSurvey activity',
 
@@ -82,13 +84,16 @@ var LimeSurveyPainter = {
 
 	fullyPaintActivity: function(activity){
 		let tmp = this;
+		activity.tmp = {};
 		this.paintActivity(activity, participants);
 
 		Simva.getActivityCompletion(activity._id, function(error, result){
+			activity.tmp.completion = result;
 			tmp.paintActivityCompletion(activity, result);
 		});
 
 		Simva.getActivityResult(activity._id, function(error, result){
+			activity.tmp.result = result;
 			tmp.paintActivityResult(activity, result);
 		});
 	},
@@ -97,6 +102,9 @@ var LimeSurveyPainter = {
 		$('#test_' + activity.test + ' .activities').append('<div id="activity_' + activity._id + '" class="activity t' + activity.type + '">'
 			+ '<div class="top"><h4>' + activity.name + '</h4>'
 			+ '<input class="red" type="button" value="X" onclick="deleteActivity(\'' + activity._id + '\')"></div>'
+			+ '<p>Survey ID: <a target="_blank" href="' + this.limesurveyurl + activity.extra_data.surveyId + '">' + activity.extra_data.surveyId + '</a></p>'
+			+ '<div id="completion_progress_' + activity._id + '" class="progress"><div class="partial"></div><div class="done"></div><span>Completed: <done>0</done>%</span></div>'
+			+ '<div id="result_progress_' + activity._id + '" class="progress"><div class="partial"></div><div class="done"></div><div></div><span>Results: <partial>0</partial>(<done>0</done>)%</span></div>'
 			+ this.paintActivityParticipantsTable(activity, participants) + '</div>');
 	},
 
@@ -104,7 +112,8 @@ var LimeSurveyPainter = {
 		let toret = '<table><tr><th>User</th><th>Completed</th><th>Result</th></tr>';
 
 		for (var i = 0; i < participants.length; i++) {
-			toret += '<tr><td>' + participants[i].username + '</td>'
+			toret += '<tr><td><a target="_blank" href="' + this.limesurveyurl + activity.extra_data.surveyId + '?token=' + participants[i].username + '">'
+				+ participants[i].username + '</a></td>'
 				+ '<td id="completion_' + activity._id + '_' + participants[i].username + '">---</td>'
 				+ '<td id="result_' + activity._id + '_' + participants[i].username + '">---</td>';
 		}
@@ -117,21 +126,71 @@ var LimeSurveyPainter = {
 	paintActivityCompletion: function(activity, status){
 		let usernames = Object.keys(status);
 
+		let done = 0;
+
 		for (var i = 0; i < usernames.length; i++) {
-			let completion = '<span class="' + ( !status[usernames[i]] ? 'red' : 'green') + '">' + status[usernames[i]] + '</span>'
+			if(status[usernames[i]]){
+				done++;
+			}
+
+			let completion = '<span>' + status[usernames[i]] + '</span>'
+			$('#completion_' + activity._id + '_' + usernames[i]).addClass(!status[usernames[i]] ? 'red' : 'green');
 			$('#completion_' + activity._id + '_' + usernames[i]).empty();
 			$('#completion_' + activity._id + '_' + usernames[i]).append(completion);
 		}
+
+		let progress = Math.round((done / usernames.length) * 1000) / 10; 
+
+		if(isNaN(progress)){
+			progress = 0;
+		}
+
+		$('#completion_progress_' + activity._id + ' .done').css('width', progress + '%' );
+		$('#completion_progress_' + activity._id + ' done').text(progress);
 	},
 
 	paintActivityResult: function(activity, results){
 		let usernames = Object.keys(results);
 
+		let done = 0, partial = 0;
+		
 		for (var i = 0; i < usernames.length; i++) {
-			let completion = '<span class="' + (results[usernames[i]] == 'No results' ? 'red' : 'green') + '">' + results[usernames[i]] + '</span>'
+
+			let color = 'red';
+			let state = 'No Results';
+
+			if(results[usernames[i]]){
+				partial++;
+				if(results[usernames[i]].submitdate){
+					color = 'green';
+					state = 'Completed';
+					done++;
+				}else{
+					color = 'yellow';
+					state = 'Started';
+				}
+			}
+
+			let completion = '<span>' + state + '</span>'
+			$('#result_' + activity._id + '_' + usernames[i]).addClass(color);
 			$('#result_' + activity._id + '_' + usernames[i]).empty();
 			$('#result_' + activity._id + '_' + usernames[i]).append(completion);
 		}
+
+		let progress = Math.round((done / usernames.length) * 1000) / 10; 
+		let partialprogress = Math.round((partial / usernames.length) * 1000) / 10;
+
+		if(isNaN(progress)){
+			progress = 0;
+		}
+		if(isNaN(partialprogress)){
+			partialprogress = 0;
+		}
+
+		$('#result_progress_' + activity._id + ' .done').css('width', progress + '%' );
+		$('#result_progress_' + activity._id + ' .partial').css('width', partialprogress + '%' );
+		$('#result_progress_' + activity._id + ' done').text(progress);
+		$('#result_progress_' + activity._id + ' partial').text(partialprogress);
 	}
 }
 
