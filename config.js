f068da12464164f5f4e33f0944f047ead857668e
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2020-09-11 14:02:16 +0200

    Use development service DNS names

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ let default_protocol_ports = {
 
 config.simva = {}
 config.simva.port  = parseInt(process.env.SIMVA_PORT || 3050);
-config.simva.host = process.env.SIMVA_HOST || 'simva.e-ucm.es'
+config.simva.host = process.env.SIMVA_HOST || 'simva.external.test'
 config.simva.protocol = process.env.SIMVA_PROTOCOL || 'https'
 config.simva.url = config.simva.protocol + '://' + config.simva.host
 				+ ( (default_protocol_ports[config.simva.protocol] !== config.simva.port) ? ':' + config.simva.port : '' );
 
 config.sso = {}
-config.sso.host = process.env.SSO_HOST || 'sso.simva.e-ucm.es'
+config.sso.host = process.env.SSO_HOST || 'sso.external.test'
 config.sso.protocol = process.env.SSO_PROTOCOL || 'https'
 config.sso.port = process.env.SSO_PORT || '443'
 config.sso.url = config.sso.protocol + '://' + config.sso.host + ':' + config.sso.port
@@ -27,13 +27,13 @@ config.sso.sslRequired = process.env.SSO_SSL_REQUIRED || 'external'
 config.sso.publicClient = process.env.SSO_PUBLIC_CLIENT || 'false'
 
 config.api = {}
-config.api.host = process.env.SIMVA_API_HOST || 'api.simva.e-ucm.es'
+config.api.host = process.env.SIMVA_API_HOST || 'simva-api.external.test'
 config.api.protocol = process.env.SIMVA_API_PROTOCOL || 'https'
 config.api.port = process.env.SIMVA_API_PORT || '443'
 config.api.url = config.api.protocol + '://' + config.api.host + ':' + config.api.port;
 
 config.limesurvey = {}
-config.limesurvey.host = process.env.LIMESURVEY_HOST || 'limesurvey-dev.external.test'
+config.limesurvey.host = process.env.LIMESURVEY_HOST || 'limesurvey.external.test'
 config.limesurvey.protocol = process.env.LIMESURVEY_PROTOCOL || 'https'
 config.limesurvey.port = process.env.LIMESURVEY_PORT || '443'
 config.limesurvey.url =  config.limesurvey.protocol + '://' + config.limesurvey.host + ':' + config.limesurvey.port
