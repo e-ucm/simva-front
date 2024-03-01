9201ad76d9bccb2dadc5866dfe3a1998c8e7a45a
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-03-01 13:41:41 +0100

    fix get role from JWT

--- routes/lib/usertools.js
+++ routes/lib/usertools.js
@@ -7,14 +7,12 @@ let querystring = require('querystring');
 
 module.exports = {
 	setUser: function(req, user){
-		console.log("USER : " + JSON.stringify(user));
 		let decoded = jwt.decode(user.jwt);
 		console.log("JWT : " + JSON.stringify(decoded));
-		if (user.data.provider != 'simva') {
 		user.data.roles = decoded.realm_access.roles;
 		user.data.role = this.getRoleFromJWT(decoded);
-		}
 		req.session.user = user;
+		console.log("USER : " + JSON.stringify(user));
 	},
 
 	getProfileFromJWT: function(token){
@@ -27,22 +25,16 @@ module.exports = {
 		profile.email = simvaJwtToken.email;
 		profile.roles = simvaJwtToken.realm_access.roles;
 		profile.role = this.getRoleFromJWT(simvaJwtToken);
-
 		return profile;
 	},
 
 	getRoleFromJWT: function(decoded){
-		console.log("Role : " + JSON.stringify(decoded));
 		let role = 'norole';
-
-		for (var i = decoded.realm_access.roles.length - 1; i >= 0; i--) {
-			if(decoded.realm_access.roles[i] === 'teacher' || decoded.realm_access.roles[i] === 'researcher'){
-				role = 'teacher';
-			} else if(decoded.realm_access.roles[i] === 'teaching-assistant' || decoded.realm_access.roles[i] === 'student'){
-				role = 'student';
-			};
-		}
-
+		if(decoded.realm_access.roles.includes('teacher') || decoded.realm_access.roles.includes('researcher')){
+			role = 'teacher';
+		} else if(decoded.realm_access.roles.includes('teaching-assistant') || decoded.realm_access.roles.includes('student')){
+			role = 'student';
+		};
 		return role;
 	}
 }
