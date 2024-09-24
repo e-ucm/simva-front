12b3fcaf0420b4ab822456d427b23b3c78da59f9
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-09-24 15:37:41 +0200

    adding administrator role

--- routes/lib/usertools.js
+++ routes/lib/usertools.js
@@ -57,7 +57,9 @@ module.exports = {
 
 	getRoleFromJWT: function(decoded){
 		let role = 'norole';
-		if(decoded.realm_access.roles.includes('teacher') || decoded.realm_access.roles.includes('researcher')){
+		if(userdata.realm_access.roles.includes('administrator')){
+			role = 'administrator';
+		} else if(decoded.realm_access.roles.includes('teacher') || decoded.realm_access.roles.includes('researcher')){
 			role = 'teacher';
 		} else if(decoded.realm_access.roles.includes('teaching-assistant') || decoded.realm_access.roles.includes('student')){
 			role = 'student';
