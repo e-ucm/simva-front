e63aa80b9e46e7c5a72e804a8c4ada08cbd5a442
Julio SANTILARIO BERTHILIER <jsantilariob@gmail.com>
2025-04-09 09:47:49 +0200

    adding /print for group and wait until loaded

--- routes/routes/previous-groups.js
+++ routes/routes/previous-groups.js
@@ -11,5 +11,9 @@ module.exports = function(auth, config){
     res.render('previous_group_view', { config: config, user: req.session.user, group: req.params['groupid'] });
   });
 
+  router.get('/:groupid/print', auth, function(req, res, next) {
+    res.render('new_group_view', { config: config, user: req.session.user, group: req.params['groupid'] });
+  });
+  
   return router;
 }
