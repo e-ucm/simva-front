53774c05a65534b1813eafd5bf47ff4dae31dd51
MattCastUCM <mattcast@ucm.es>
2026-06-30 10:23:55 +0000

    Fix filtering event handling for non-chromium based browsers

--- public/tooltips.js
+++ public/tooltips.js
@@ -7,8 +7,8 @@ $(document).on("mouseenter", ".tooltip", function (e) {
     const tooltip = $(this);
     const tooltiptext = tooltip.find(".tooltiptext");
 
-    const tooltipRelativeX = tooltip.offset().left - $(window).scrollLeft();
-    const tooltipRelativeY = tooltip.offset().top - $(window).scrollTop();
+    const tooltipRelativeX = tooltip.offset().left - window.scrollX;
+    const tooltipRelativeY = tooltip.offset().top - window.scrollY;
 
     let left = tooltipRelativeX + OFFSET_X;
     let top = tooltipRelativeY + OFFSET_Y;
@@ -19,12 +19,12 @@ $(document).on("mouseenter", ".tooltip", function (e) {
     let out = false;
     
     // If the text is horizontally out of the window, put it on the left of the icon
-    if (tooltiptext[0].getBoundingClientRect().right > $(window).width()) {
+    if (tooltiptext[0].getBoundingClientRect().right > window.innerWidth) {
         left = tooltipRelativeX - tooltiptext.width() - OFFSET_X;
         out = true;
     }
     // If the text is vertically out of the window, put it on top of the icon
-    if (tooltiptext[0].getBoundingClientRect().bottom > $(window).height()) {
+    if (tooltiptext[0].getBoundingClientRect().bottom > window.innerHeight) {
         top = tooltipRelativeY - tooltiptext.height() - OFFSET_Y;
         out = true;
     }
